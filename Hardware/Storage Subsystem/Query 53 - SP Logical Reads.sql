63de8a731c4c7851b5d67216e9f67f4da9b63cb1
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-03-07 10:19:34 +0700

    Query 53 - SP Logical Reads

---
+++ Hardware/Storage Subsystem/Query 53 - SP Logical Reads.sql
@@ -0,0 +1,15 @@
+-- Top Cached SPs By Total Logical Reads (SQL Server 2014) (Query 53) (SP Logical Reads)
+-- Logical reads relate to memory pressure  
+SELECT TOP(25) p.name AS [SP Name], qs.total_logical_reads AS [TotalLogicalReads], 
+qs.total_logical_reads/qs.execution_count AS [AvgLogicalReads],qs.execution_count, 
+ISNULL(qs.execution_count/DATEDIFF(Minute, qs.cached_time, GETDATE()), 0) AS [Calls/Minute], 
+qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count 
+AS [avg_elapsed_time], qs.cached_time
+FROM sys.procedures AS p WITH (NOLOCK)
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
+ON p.[object_id] = qs.[object_id]
+WHERE qs.database_id = DB_ID()
+ORDER BY qs.total_logical_reads DESC OPTION (RECOMPILE);
+
+-- This helps you find the most expensive cached stored procedures from a memory perspective
+-- You should look at this if you see signs of memory pressure
