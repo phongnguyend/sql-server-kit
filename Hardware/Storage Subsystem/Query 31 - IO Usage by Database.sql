46c6da42425e24d9af3749218e65711569ff82b8
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-03-07 10:31:44 +0700

    Query 31 - IO Usage by Database

---
+++ Hardware/Storage Subsystem/Query 31 - IO Usage by Database.sql
@@ -0,0 +1,13 @@
+-- Get I/O utilization by database (Query 31) (IO Usage By Database)
+WITH Aggregate_IO_Statistics
+AS
+(SELECT DB_NAME(database_id) AS [Database Name],
+CAST(SUM(num_of_bytes_read + num_of_bytes_written)/1048576 AS DECIMAL(12, 2)) AS io_in_mb
+FROM sys.dm_io_virtual_file_stats(NULL, NULL) AS [DM_IO_STATS]
+GROUP BY database_id)
+SELECT ROW_NUMBER() OVER(ORDER BY io_in_mb DESC) AS [I/O Rank], [Database Name], io_in_mb AS [Total I/O (MB)],
+       CAST(io_in_mb/ SUM(io_in_mb) OVER() * 100.0 AS DECIMAL(5,2)) AS [I/O Percent]
+FROM Aggregate_IO_Statistics
+ORDER BY [I/O Rank] OPTION (RECOMPILE);
+
+-- Helps determine which database is using the most I/O resources on the instance
