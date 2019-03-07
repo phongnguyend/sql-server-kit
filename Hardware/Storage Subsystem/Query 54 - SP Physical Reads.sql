4b549b1f6a7357187e0d844bae8c64d6e3120ebc
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-03-07 10:19:57 +0700

    Query 54 - SP Physical Reads

---
+++ Hardware/Storage Subsystem/Query 54 - SP Physical Reads.sql
@@ -0,0 +1,15 @@
+-- Top Cached SPs By Total Physical Reads (SQL Server 2014) (Query 54) (SP Physical Reads)
+-- Physical reads relate to disk I/O pressure  
+SELECT TOP(25) p.name AS [SP Name],qs.total_physical_reads AS [TotalPhysicalReads], 
+qs.total_physical_reads/qs.execution_count AS [AvgPhysicalReads], qs.execution_count, 
+qs.total_logical_reads,qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count 
+AS [avg_elapsed_time], qs.cached_time 
+FROM sys.procedures AS p WITH (NOLOCK)
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
+ON p.[object_id] = qs.[object_id]
+WHERE qs.database_id = DB_ID()
+AND qs.total_physical_reads > 0
+ORDER BY qs.total_physical_reads DESC, qs.total_logical_reads DESC OPTION (RECOMPILE);
+
+-- This helps you find the most expensive cached stored procedures from a read I/O perspective
+-- You should look at this if you see signs of I/O pressure or of memory pressure
