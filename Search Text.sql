b3eb6c2a3fb48851ecd1acd101336fe3e9e9d508
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-03-08 23:59:11 +0700

    Utilities

--- Search Text.sql
+++
@@ -1,4 +0,0 @@
-SELECT o.name, o.xtype, m.definition, m.uses_ansi_nulls, m.uses_quoted_identifier
-FROM sys.sql_modules  m  
-INNER JOIN sysobjects o ON m.object_id = o.id 
-WHERE m.definition LIKE '%xxx%' 
