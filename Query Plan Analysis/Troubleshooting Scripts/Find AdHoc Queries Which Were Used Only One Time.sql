ab3e49b914550e139b5c8bf4822f193f0aecd60c
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-03-12 13:53:47 +0700

    Update and rename Query 45 - Ad Hoc Queries.sql to Find AdHoc Queries Which Were Used Only One Time.sql

--- Query Plan Analysis/Troubleshooting Scripts/Find AdHoc Queries Which Were Used Only One Time.sql
+++ Query Plan Analysis/Troubleshooting Scripts/Find AdHoc Queries Which Were Used Only One Time.sql
@@ -1,7 +1,9 @@
 -- Find single-use, ad-hoc and prepared queries that are bloating the plan cache  (Query 44) (Ad hoc Queries)
-SELECT TOP(50) [text] AS [QueryText], cp.cacheobjtype, cp.objtype, cp.size_in_bytes 
+SELECT TOP(100) [text] AS [QueryText], cp.cacheobjtype, cp.objtype, cp.size_in_bytes
+, qs.creation_time, qs.last_execution_time, qs.execution_count 
 FROM sys.dm_exec_cached_plans AS cp WITH (NOLOCK)
-CROSS APPLY sys.dm_exec_sql_text(plan_handle) 
+CROSS APPLY sys.dm_exec_sql_text(plan_handle)
+LEFT JOIN sys.dm_exec_query_stats AS qs on cp.plan_handle = qs.plan_handle
 WHERE cp.cacheobjtype = N'Compiled Plan' 
 AND cp.objtype IN (N'Adhoc', N'Prepared') 
 AND cp.usecounts = 1
