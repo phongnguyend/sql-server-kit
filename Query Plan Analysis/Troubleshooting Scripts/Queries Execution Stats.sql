55bc1d55c2bf02ea9610c8662055e79d50d61ff7
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-03-23 12:12:40 +0700

    Update Queries Execution Stats.sql

--- Query Plan Analysis/Troubleshooting Scripts/Queries Execution Stats.sql
+++ Query Plan Analysis/Troubleshooting Scripts/Queries Execution Stats.sql
@@ -1,5 +1,6 @@
 SELECT TOP (100)
-	SUBSTRING(qt.TEXT, qs.statement_start_offset / 2 + 1, (
+	OBJECT_NAME(qt.objectid, qt.dbid) AS [ProcedureName]
+	,SUBSTRING(qt.TEXT, qs.statement_start_offset / 2 + 1, (
 			CASE 
 				WHEN qs.statement_end_offset = - 1
 					THEN LEN(CONVERT(NVARCHAR(MAX), qt.TEXT)) * 2
@@ -7,33 +8,50 @@ SELECT TOP (100)
 				END - qs.statement_start_offset
 			) / 2) AS query_text
 	,qs.execution_count
+	,qs.creation_time
+	,qs.last_execution_time
 	,qs.total_rows
 	,qs.min_rows
 	,qs.max_rows
 	,qs.last_rows
-	,qs.min_elapsed_time
-	,qs.max_elapsed_time
-	,qs.last_elapsed_time
-	,qs.total_elapsed_time / qs.execution_count AS [avg_elapsed_time]
+	-- Elapsed Time
 	,qs.min_elapsed_time / cast(1000000 AS FLOAT) AS [min_elapsed_time_seconds]
 	,qs.max_elapsed_time / cast(1000000 AS FLOAT) AS [max_elapsed_time_seconds]
 	,qs.last_elapsed_time / cast(1000000 AS FLOAT) AS [last_elapsed_time_seconds]
 	,qs.total_elapsed_time / qs.execution_count / cast(1000000 AS FLOAT) AS [avg_elapsed_time_seconds]
-	,total_worker_time
-	,total_logical_reads
+	-- Worker Time
+	,qs.min_worker_time / cast(1000000 AS FLOAT) AS [min_worker_time_seconds]
+	,qs.max_worker_time / cast(1000000 AS FLOAT) AS [max_worker_time_seconds]
+	,qs.last_worker_time / cast(1000000 AS FLOAT) AS [last_worker_time_seconds]
+	,qs.total_worker_time / qs.execution_count / cast(1000000 AS FLOAT) AS [avg_worker_time_seconds]
+	-- Logical Reads
+	,qs.min_logical_reads
+	,qs.max_logical_reads
+	,qs.last_logical_reads
+	,qs.total_logical_reads
+	,qs.total_logical_reads / qs.execution_count as [avg_logical_reads]
+	-- Physical Reads
+	,qs.min_physical_reads
+	,qs.max_physical_reads
+	,qs.last_physical_reads
+	,qs.total_physical_reads
+	,qs.total_physical_reads / qs.execution_count as [avg_physical_reads]
+	-- Logical Writes
+	,qs.min_logical_writes
+	,qs.max_logical_writes
+	,qs.last_logical_writes
+	,qs.total_logical_writes
+	,qs.total_logical_writes / qs.execution_count as [avg_logical_writes]
+	-- AVG IO
+	,(qs.total_logical_reads + qs.total_logical_writes) / qs.execution_count AS [Avg IO]
 	,qs.plan_handle
-	--,qp.query_plan
 FROM sys.dm_exec_query_stats AS qs WITH (NOLOCK)
 CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt
---CROSS APPLY sys.dm_exec_query_plan (qs.plan_handle) AS qp
 WHERE qt.dbid = DB_ID()
 ORDER BY qs.execution_count DESC
 OPTION (RECOMPILE);
-		
+
 		
 /*
-		
 select * from  sys.dm_exec_query_plan (0x05000900784D9104105771230100000001000000000000000000000000000000000000000000000000000000)
-
 */
-
