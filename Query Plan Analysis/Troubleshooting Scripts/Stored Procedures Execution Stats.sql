3c6f5ff05ac1060532fa592014c74ddb6ac060dc
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-03-23 12:18:52 +0700

    Update Stored Procedures Execution Stats.sql

--- Query Plan Analysis/Troubleshooting Scripts/Stored Procedures Execution Stats.sql
+++ Query Plan Analysis/Troubleshooting Scripts/Stored Procedures Execution Stats.sql
@@ -1,14 +1,42 @@
-SELECT TOP (25) p.NAME AS [SP Name]
-	,qs.total_elapsed_time / qs.execution_count / cast(1000000 AS FLOAT) AS [avg_elapsed_time_seconds]
-	,qs.total_elapsed_time / cast(1000000 AS FLOAT) AS [total_elapsed_time_seconds]
+SELECT TOP (25) 
+	 p.NAME AS [SP Name]
 	,qs.execution_count
 	,qs.cached_time
-	,qs.min_elapsed_time / cast(1000000 AS FLOAT) AS min_elapsed_time_seconds
-	,qs.max_elapsed_time / cast(1000000 AS FLOAT) AS max_elapsed_time_seconds
-	,qs.last_elapsed_time / cast(1000000 AS FLOAT) AS last_elapsed_time_seconds
+	-- Elapsed Time
+	,qs.min_elapsed_time / cast(1000000 AS FLOAT) AS [min_elapsed_time_seconds]
+	,qs.max_elapsed_time / cast(1000000 AS FLOAT) AS [max_elapsed_time_seconds]
+	,qs.last_elapsed_time / cast(1000000 AS FLOAT) AS [last_elapsed_time_seconds]
+	,qs.total_elapsed_time / cast(1000000 AS FLOAT) AS [total_elapsed_time_seconds]
+	,qs.total_elapsed_time / qs.execution_count / cast(1000000 AS FLOAT) AS [avg_elapsed_time_seconds]
+	-- Worker Time
+	,qs.min_worker_time / cast(1000000 AS FLOAT) AS [min_worker_time_seconds]
+	,qs.max_worker_time / cast(1000000 AS FLOAT) AS [max_worker_time_seconds]
+	,qs.last_worker_time / cast(1000000 AS FLOAT) AS [last_worker_time_seconds]
+	,qs.total_worker_time / cast(1000000 AS FLOAT) AS [total_worker_time_seconds]
+	,qs.total_worker_time / qs.execution_count / cast(1000000 AS FLOAT) AS [avg_worker_time_seconds]
+	-- Logical Reads
+	,qs.min_logical_reads
+	,qs.max_logical_reads
+	,qs.last_logical_reads
+	,qs.total_logical_reads
+	,qs.total_logical_reads / qs.execution_count as [avg_logical_reads]
+	-- Physical Reads
+	,qs.min_physical_reads
+	,qs.max_physical_reads
+	,qs.last_physical_reads
+	,qs.total_physical_reads
+	,qs.total_physical_reads / qs.execution_count as [avg_physical_reads]
+	-- Logical Writes
+	,qs.min_logical_writes
+	,qs.max_logical_writes
+	,qs.last_logical_writes
+	,qs.total_logical_writes
+	,qs.total_logical_writes / qs.execution_count as [avg_logical_writes]
+	-- AVG IO
+	,(qs.total_logical_reads + qs.total_logical_writes) / qs.execution_count AS [Avg IO]
 	,qs.plan_handle
 FROM sys.procedures AS p WITH (NOLOCK)
-INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK) ON p.[object_id] = qs.[object_id]
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK) ON p.object_id = qs.object_id
 WHERE qs.database_id = DB_ID()
 ORDER BY [avg_elapsed_time_seconds] DESC
 --ORDER BY qs.execution_count DESC
