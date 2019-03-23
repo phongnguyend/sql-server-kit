6686ac8662cfd798c5532765d1e65904b1b19310
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-03-23 16:33:15 +0700

    Delete Check DB Index Fragmentation.sql

--- Indexes/Check DB Index Fragmentation.sql
+++
@@ -1,8 +0,0 @@
-SELECT a.index_id
-,name
-,avg_fragmentation_in_percent
-,OBJECT_NAME(a.object_id) as TableName
-FROM sys.dm_db_index_physical_stats (DB_ID(),null, NULL, NULL, NULL) AS a  
-JOIN sys.indexes AS b ON a.object_id = b.object_id AND a.index_id = b.index_id
-where avg_fragmentation_in_percent > 30
-order by avg_fragmentation_in_percent desc
