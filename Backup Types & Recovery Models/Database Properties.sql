7700b17e9102bb721c85385afd8f8fcb31e7b694
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-08-05 10:31:19 +0700

    Backup & Restore

---
+++ Backup Types & Recovery Models/Database Properties.sql
@@ -0,0 +1,30 @@
+-- Recovery model, log reuse wait description, log file size, log usage size  (Query 27) (Database Properties)
+-- and compatibility level for all databases on instance
+SELECT db.[name] AS [Database Name], db.recovery_model_desc AS [Recovery Model], db.state_desc, 
+db.log_reuse_wait_desc AS [Log Reuse Wait Description], 
+CONVERT(DECIMAL(18,2), ls.cntr_value/1024.0) AS [Log Size (MB)], CONVERT(DECIMAL(18,2), lu.cntr_value/1024.0) AS [Log Used (MB)],
+CAST(CAST(lu.cntr_value AS FLOAT) / CAST(ls.cntr_value AS FLOAT)AS DECIMAL(18,2)) * 100 AS [Log Used %], 
+db.[compatibility_level] AS [DB Compatibility Level], db.page_verify_option_desc AS [Page Verify Option], 
+db.is_auto_create_stats_on, db.is_auto_update_stats_on, db.is_auto_update_stats_async_on, db.is_parameterization_forced, 
+db.snapshot_isolation_state_desc, db.is_read_committed_snapshot_on, db.is_auto_close_on, db.is_auto_shrink_on, 
+db.target_recovery_time_in_seconds, db.is_cdc_enabled, db.is_memory_optimized_elevate_to_snapshot_on, db.delayed_durability_desc, 
+db.is_auto_create_stats_incremental_on      
+FROM sys.databases AS db WITH (NOLOCK)
+INNER JOIN sys.dm_os_performance_counters AS lu WITH (NOLOCK)
+ON db.name = lu.instance_name
+INNER JOIN sys.dm_os_performance_counters AS ls WITH (NOLOCK)
+ON db.name = ls.instance_name
+WHERE lu.counter_name LIKE N'Log File(s) Used Size (KB)%' 
+AND ls.counter_name LIKE N'Log File(s) Size (KB)%'
+AND ls.cntr_value > 0 
+ORDER BY db.[name] OPTION (RECOMPILE);
+
+-- Things to look at:
+-- How many databases are on the instance?
+-- What recovery models are they using?
+-- What is the log reuse wait description?
+-- How full are the transaction logs ?
+-- What compatibility level are the databases on? 
+-- What is the Page Verify Option? (should be CHECKSUM)
+-- Is Auto Update Statistics Asynchronously enabled?
+-- Make sure auto_shrink and auto_close are not enabled!
---
+++ Backup Types & Recovery Models/Getting VLF Counts.sql
@@ -0,0 +1,30 @@
+-- Get VLF Counts for all databases on the instance (Query 29) (VLF Counts)
+-- (adapted from Michelle Ufford) 
+CREATE TABLE #VLFInfo (RecoveryUnitID int, FileID  int,
+					   FileSize bigint, StartOffset bigint,
+					   FSeqNo      bigint, [Status]    bigint,
+					   Parity      bigint, CreateLSN   numeric(38));
+	 
+CREATE TABLE #VLFCountResults(DatabaseName sysname, VLFCount int);
+	 
+EXEC sp_MSforeachdb N'Use [?]; 
+
+				INSERT INTO #VLFInfo 
+				EXEC sp_executesql N''DBCC LOGINFO([?])''; 
+	 
+				INSERT INTO #VLFCountResults 
+				SELECT DB_NAME(), COUNT(*) 
+				FROM #VLFInfo; 
+
+				TRUNCATE TABLE #VLFInfo;'
+	 
+SELECT DatabaseName, VLFCount  
+FROM #VLFCountResults
+ORDER BY VLFCount DESC;
+	 
+DROP TABLE #VLFInfo;
+DROP TABLE #VLFCountResults;
+
+-- High VLF counts can affect write performance 
+-- and they can make database restores and recovery take much longer
+-- Try to keep your VLF counts under 200 in most cases
