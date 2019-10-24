d3c425a70a524662db6b0087a4fa2ae25932b775
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-10-24 11:39:44 +0700

    Update Recent Full Backups.sql

--- Backup Types & Recovery Models/Recent Full Backups.sql
+++ Backup Types & Recovery Models/Recent Full Backups.sql
@@ -5,7 +5,8 @@ CONVERT (BIGINT, bs.compressed_backup_size / 1048576 ) AS [Compressed Backup Siz
 CONVERT (NUMERIC (20,2), (CONVERT (FLOAT, bs.backup_size) /
 CONVERT (FLOAT, bs.compressed_backup_size))) AS [Compression Ratio], 
 DATEDIFF (SECOND, bs.backup_start_date, bs.backup_finish_date) AS [Backup Elapsed Time (sec)],
-bs.backup_finish_date AS [Backup Finish Date]
+bs.backup_finish_date AS [Backup Finish Date],
+bs.is_copy_only
 FROM msdb.dbo.backupset AS bs WITH (NOLOCK)
 WHERE DATEDIFF (SECOND, bs.backup_start_date, bs.backup_finish_date) > 0 
 AND bs.backup_size > 0
