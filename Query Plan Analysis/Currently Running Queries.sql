76d0ef0172925c621907906eab3d21cf6ad29915
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-07-12 16:23:59 +0700

    Update Currently Running Queries.sql

--- Query Plan Analysis/Currently Running Queries.sql
+++ Query Plan Analysis/Currently Running Queries.sql
@@ -6,6 +6,7 @@ SELECT DB_NAME(req.database_id) AS db_name
 	,req.start_time
 	,req.cpu_time
 	,req.total_elapsed_time
+	,req.plan_handle
 	,queryplan.query_plan
 	,c.client_net_address
 	,s.login_name
@@ -23,3 +24,13 @@ JOIN sys.dm_exec_connections c ON req.connection_id = c.connection_id
 JOIN sys.dm_exec_sessions s ON req.session_id = s.session_id
 CROSS APPLY sys.dm_exec_sql_text(sql_handle) AS sqltext
 CROSS APPLY sys.dm_exec_query_plan(req.plan_handle) AS queryplan
+
+/*
+
+-- Kill session_id
+KILL session_id;
+
+-- Remove the specific query plan from the cache using the plan handle from the above query 
+DBCC FREEPROCCACHE (0x050011007A2CC30E204991F30200000001000000000000000000000000000000000000000000000000000000);
+
+*/
