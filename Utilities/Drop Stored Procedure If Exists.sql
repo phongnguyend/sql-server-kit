413d81dcc363169117fec44adfe934e63eadb56a
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-07-02 07:06:59 +0700

    Update Drop Stored Procedure If Exists.sql

--- Utilities/Drop Stored Procedure If Exists.sql
+++ Utilities/Drop Stored Procedure If Exists.sql
@@ -1,12 +1,4 @@
-IF EXISTS (
-		SELECT *
-		FROM sys.objects
-		WHERE object_id = OBJECT_ID(N'[schema].[StoredProcedureName]')
-			AND type IN (
-				N'P'
-				,N'PC'
-				)
-		)
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[schema].[StoredProcedureName]') AND type IN (N'P', N'PC'))
 BEGIN
 	DROP PROCEDURE [schema].[StoredProcedureName]
 END
